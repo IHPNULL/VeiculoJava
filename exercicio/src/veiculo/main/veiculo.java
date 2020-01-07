22af87eb8f2019016a64113aa6f49d291a319aca
italo haas pascoli <54510120+IHPNULL@users.noreply.github.com>
2020-01-07 09:38:52 -0300

    first commit

---
+++ exercicio/src/veiculo/main/veiculo.java
@@ -0,0 +1,10 @@
+package veiculo.main;
+
+public class veiculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ exercicio/src/veiculo/model/funcoes.java
@@ -0,0 +1,10 @@
+package veiculo.model;
+
+public class funcoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
